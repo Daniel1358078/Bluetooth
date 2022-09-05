597323bfd8891d820c676351070c168207966d37
[Daniel1358078] <[1358078@clases.edu.sv]>
2022-09-05 15:51:21 -0600

    bluetooth

---
+++ src/main/java/com/mycompany/bluetooth/Bluetooth.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.bluetooth;
+
+/**
+ *
+ * @author daniel
+ */
+public class Bluetooth {
+
+    public static void main(String[] args) {
+
+        boolean isBluetoothEnabled = false;
+        
+        if (isBluetoothEnabled) {
+            int fileSended = 0;
+            fileSended++;
+            System.out.println("Archivo enviado");
+        } else {
+           System.out.println("El Bluetooth no esta activdo");
+        }
+        
+    }
+}
